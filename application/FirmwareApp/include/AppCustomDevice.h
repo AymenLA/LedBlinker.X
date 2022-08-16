9b8c92ecba15240764bae81da9ff166de72fa336
AymenLa <aymen.laouini@ymail.com>
2022-08-16 11:07:09 +0200

    updating file names for the compilation exemple

---
+++ application/FirmwareApp/include/AppCustomDevice.h
@@ -0,0 +1,14 @@
+#ifndef _APPCUSTOMDEVICE_H_
+#define	_APPCUSTOMDEVICE_H_
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+void APP_DeviceTasks(void);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* _APPCUSTOMDEVICE_H_ */
